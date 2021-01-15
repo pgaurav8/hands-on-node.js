212074a909f7de18ad22b16e433cd32197419a4b
Parmar <gaurav.parmar@eronkan.com>
2021-01-15 17:14:50 +0530

    Client Multicast Example

---
+++ ch14/multicast_client.js
@@ -0,0 +1,15 @@
+var dgram = require('dgram');
+
+var client = dgram.createSocket('udp4');
+
+var message = new Buffer('this is a multicast message');
+
+client.send(message, 0, message.length, 4000, '230.1.2.3'), function(){
+    client.setMulticastTTL(10);
+};
+
+
+
+
+
+client.close();
