1ceb9c9cf5f687d40bdb30deebf6cd01d0ee6feb
Parmar <gaurav.parmar@eronkan.com>
2021-01-18 18:24:05 +0530

    Main File

--- ch21/app.js
+++ ch21/app.js
@@ -1,7 +1,24 @@
+require('should');
 var sum = require('./module1');
+var asyn = require('./module2');
 
-console.log('Sum of 13 and -21 is : ' + sum(13,(-21)) + '  /-');
+describe("Sum Func. and asynch. Test : \n", function(){
+    it('should be sum 0,5', function(){
+        sum(0,5).should.equal(5);
+    });
 
-var mod2 = require('./module2');
+    it('should be sum 2,5', function(){
+        sum(2,5).should.equal(7);
+    });
 
-console.log(' \n\nTesting Asynchronously : \n\n' + mod2.testAsync())
+    it('should be sum 1,1 . 1,2 . 2,1 . 10,120 :', function(){
+        sum(1,1).should.equal(2);
+        sum(1,2).should.equal(3);
+        sum(2,1).should.equal(3);
+        sum(10,120).should.equal(130);
+    });
+
+    it('Testing Asynchronous function test : ', function(){
+        asyn.testAsync().should.equal(2);
+    })
+})
